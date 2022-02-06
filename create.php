29d2927e8b500c25a9d0d0e5fcc2ec3e3758fce2
はんどくりーむ <99105005+handcreamde@users.noreply.github.com>
2022-02-06 10:19:17 +0900

    Create create.php

---
+++ create.php
@@ -0,0 +1,16 @@
+<?php
+$str = file_get_contents('https://font.snskun.repl.co/fontstyle.php');
+echo $str;
+?>
+<title>フィルタリング回避システム2(.1)</title>
+<form action="create.php" method="post">
+<center><h1><u>フィルタリング回避プログラム2</u></h1></center>
+<?php
+$url = $_POST['url'];
+$durl = $_POST['durl'];
+echo '<center>URL(共有できます):<br> https://fesv.repl.co/filter2/data.php?url='.$url.'&durl='.$durl.'</p></center>';
+?>
+<center><h3>作成したURLは他人に共有できます！</h3>
+<h3>このツールを使ってもブロックされてしまった場合は、<a href="/filter-alpha/">アルファバージョン</a>を使ってみてください。</h3></center>
+<hr>
+<center><p>使用はあくまで、<b>★自★己★責★任★</b>で、おなしゃす。</p></center>
