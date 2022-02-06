d6fafc70608908d7a6e26600157ff5aeafb3f8fc
はんどくりーむ <99105005+handcreamde@users.noreply.github.com>
2022-02-06 10:18:48 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$str = file_get_contents('https://font.snskun.repl.co/fontstyle.php');
+echo $str;
+?>
+<title>フィルタリング回避システム2</title>
+<form action="create.php" method="post">
+<center><h1><u>フィルタリング回避プログラム2(.1)</u></h1></center>
+<hr>
+<h3>注意事項</h3>
+<p>このツールは随時作成中で、未完成の部分が所々あります。<br>例として一部の画像が表示されなかったり、一部テキストが表示されない場合があります。ご注意ください。</p>
+<p>そして、別のサイトに飛ぶ場合は使用できません。複数のページをノンストレスで見たい場合は、対応していないページもありますが<a href="/filter-alpha/">「アルファバージョン」</a>をご使用ください。</p>
+<p>URL</p>
+<textarea name="url" cols="100" rows="1"></textarea>
+<p>元URL(https://google.com/r49rtejg なら https://google.com/)</p>
+<textarea name="durl" cols="100" rows="1"></textarea>
+<p><input type="submit" name="send" value="作成"></p>
+</form>
+<hr>
+<center><p>使用はあくまで、<b>★自★己★責★任★</b>で、おなしゃす。</p>
+</center>
